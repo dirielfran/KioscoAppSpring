232ccd05b6b31b8a2903900bcade6529fe5e67eb
Elvis Areiza Contreras <eareizac@everis.com>
2021-02-25 21:22:44 -0300

    cambios varios

--- src/main/java/com/eareiza/springAngular/model/repository/IFacturaRepository.java
+++ src/main/java/com/eareiza/springAngular/model/repository/IFacturaRepository.java
@@ -13,18 +13,18 @@ import com.eareiza.springAngular.model.entity.Factura;
 public interface IFacturaRepository extends CrudRepository<Factura, Long> {
 	
 	@Query(value="select p.nombre as producto, "
-				+ "sum(fi.cantidad) as cantiad, "
-				+ "inv.preciocompra as precio, "
-				+ "inv.inventario_id as inventario, "
-				+ "fi.id as factura "
-			+ "from facturas_items fi "
-			+ "inner join productos p "
-			+ "	on fi.producto_id = p.id "
-			+ "inner join inventarios_items inv "
-			+ "	on inv.id = (select iteminventario_id "
-			+ "					from itemsfactura_itemsinventario "
-			+ "					where itemfactura_id = fi.id limit 1) "
-			+ "where fi.consignacion = 1 "
-			+ "group by fi.producto_id", nativeQuery = true)
+						+ "sum(fi.cantidad) as cantiad, "
+						+ "inv.preciocompra as precio, "
+						+ "inv.inventario_id as inventario, "
+						+ "fi.id as factura "
+					+ "from facturas_items fi "
+					+ "inner join productos p "
+					+ "	on fi.producto_id = p.id "
+					+ "inner join inventarios_items inv "
+					+ "	on inv.id = (select iteminventario_id "
+					+ "					from itemsfactura_itemsinventario "
+					+ "					where itemfactura_id = fi.id limit 1) "
+					+ "where fi.consignacion = 1 "
+					+ "group by fi.producto_id", nativeQuery = true)
 	public List<Object[]> findConsignaciones();	
 }
--- src/main/java/com/eareiza/springAngular/model/service/GastosServiceImpl.java
+++ src/main/java/com/eareiza/springAngular/model/service/GastosServiceImpl.java
@@ -1,6 +1,6 @@
 package com.eareiza.springAngular.model.service;
 
-import java.math.BigDecimal;
+
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
